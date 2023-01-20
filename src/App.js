e2cdd67c5a70490ca216c95d05f34e5014ee07a2
sandeepkamara12 <sandeepkamara@gmail.com>
2023-01-20 17:20:20 +0530

    create a setup for deposit and withdraw money using react-redux, redux, redux-thunk

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from './store/index';
 
 function App() {
+  const account = useSelector(state => state.account);
+  const dispatch = useDispatch();
+
+  const AC = bindActionCreators(actionCreators, dispatch);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Bank Amount Is {account}
+      <button onClick={() => AC.depositMoney(100)}>deposit</button>
+      <button onClick={() => AC.withdrawMoney(100)}>withdraw</button>
     </div>
   );
 }
